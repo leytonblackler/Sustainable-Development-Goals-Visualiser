1e69dc8a0c1ea7c9ea60932df8797045f292a959
Leyton Blackler <blacklleyt@ecs.vuw.ac.nz>
2019-09-17 12:52:47 +1200

    Tidied up CRA.

---
+++ sustainable-development-goals-visualiser/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class Main extends Component {
+
+    render() {
+        return <RootContainer>make all components children of this</RootContainer>
+    }
+
+}
+
+const RootContainer = styled.div`
+    height: 100%;
+`
