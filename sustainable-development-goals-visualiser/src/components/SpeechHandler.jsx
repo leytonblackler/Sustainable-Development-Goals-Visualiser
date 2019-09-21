83e30032b640543b46ba6166815544fabf8aa973
Leyton Blackler <leytonblackler@gmail.com>
2019-09-21 14:07:06 +1200

    Implemented detection for a trigger keyword.

--- sustainable-development-goals-visualiser/src/components/SpeechHandler.jsx
+++ sustainable-development-goals-visualiser/src/components/SpeechHandler.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import SpeechRecognition from "react-speech-recognition";
 
+const TRIGGER_WORD = "compare";
+
 class SpeechHandler extends Component {
   constructor(props) {
     super(props);
@@ -27,14 +29,21 @@ class SpeechHandler extends Component {
 
   checkTranscript = () => {
     const transcript = this.props.interimTranscript.toLowerCase();
-    // console.log("current transcript:", transcript);
-    const found = this.props.countryData.find(country => {
-      return transcript.includes(country.name.toLowerCase());
-    });
-    if (found) {
-      console.log("Detected country: ", found);
+
+    //First check to see if the trigger word has been spoken.
+    if (transcript.includes(TRIGGER_WORD.toLowerCase())) {
+      console.log("Activating compare...");
       this.props.resetTranscript();
     }
+
+    // console.log("current transcript:", transcript);
+    // const found = this.props.countryData.find(country => {
+    //   return transcript.includes(country.name.toLowerCase());
+    // });
+    // if (found) {
+    //   console.log("Detected country: ", found);
+    //   this.props.resetTranscript();
+    // }
 
     // else {
     //   console.log("Did not detect country.");
