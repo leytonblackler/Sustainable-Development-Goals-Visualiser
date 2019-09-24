d27a4e94536ad2073086e40c6d12734df9817d40
Leyton Blackler <leytonblackler@gmail.com>
2019-09-24 15:38:31 +1200

    Added same styling as notification to drawer.

--- sustainable-development-goals-visualiser/src/components/InfoDrawer.jsx
+++ sustainable-development-goals-visualiser/src/components/InfoDrawer.jsx
@@ -33,7 +33,6 @@ const containerStyle = css`
 
 const modalStyle = css`
   float: bottom;
-  background-color: red;
   height: 70vh;
   width: calc(100vw - ${2 * PADDING}px);
   border-radius: 10px;
@@ -43,10 +42,12 @@ const modalStyle = css`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
+  background-color: white;
+  opacity: 0.95;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 `;
 
 const ContentContainer = styled.div`
-  background-color: lime;
   margin-top: ${PADDING}px;
   width: calc(100% - ${2 * PADDING}px);
   height: calc(100% - ${2 * PADDING}px - ${5 * PADDING}px);
