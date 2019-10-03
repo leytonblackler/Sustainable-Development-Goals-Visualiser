75c6d9a40dbca36d24e3661008f4821d150aa751
Leyton Blackler <leytonblackler@gmail.com>
2019-10-03 13:56:15 +1300

    Improved slider styling and added step indiciators.

--- sustainable-development-goals-visualiser/src/components/Slider.jsx
+++ sustainable-development-goals-visualiser/src/components/Slider.jsx
@@ -30,6 +30,10 @@ const Slider = props => {
         }
         valueLabelDisplay="on"
       />
+      <StartEndLabelsContainer>
+        <YearLabel>{props.min}</YearLabel>
+        <YearLabel>{props.max}</YearLabel>
+      </StartEndLabelsContainer>
     </SliderContainer>
   );
 };
@@ -37,11 +41,30 @@ const Slider = props => {
 export default Slider;
 
 const SliderContainer = styled.div`
-  width: 80%;
+  width: 80vw;
   color: "#52af77";
   border: "2px solid currentColor";
   position: absolute;
   bottom: 5%;
+`;
+
+const StartEndLabelsContainer = styled.div`
+  width: calc(80vw + 10px + 20px - 3px);
+  margin-left: -20px;
+  height: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const YearLabel = styled.div`
+  color: white;
+  opacity: 0.95;
+  font-family: CircularStd;
+  font-size: 9pt;
+  text-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  font-weight: 600;
 `;
 
 const PrettoSlider = withStyles({
@@ -53,6 +76,7 @@ const PrettoSlider = withStyles({
     width: 24,
     backgroundColor: "white",
     marginTop: -7,
+    marginLeft: -17,
     boxShadow: "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)",
     border: "0px solid #3498db",
     "&:focus,&:hover,&$active": {
@@ -68,13 +92,18 @@ const PrettoSlider = withStyles({
       color: "white",
       opacity: 0.95,
       fontFamily: "CircularStd",
-      fontSize: "9pt"
+      fontSize: "9pt",
+      textShadow:
+        "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)",
+      fontWeight: 600
     }
   },
   track: {
     opacity: 0
   },
   rail: {
+    width: "calc(80vw + 10px)",
+    marginLeft: "calc(-10px)",
     backgroundColor: "white",
     opacity: 0.65,
     height: 10,
@@ -82,13 +111,11 @@ const PrettoSlider = withStyles({
     boxShadow: "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)"
   },
   mark: {
-    backgroundColor: "#3498db",
-    height: 0,
-    width: 0,
-    marginTop: 0
+    backgroundColor: "white",
+    borderRadius: 50,
+    height: 10,
+    width: 10,
+    marginTop: 0,
+    marginLeft: -10
   }
-  // markActive: {
-  //   opacity: 1,
-  //   backgroundColor: 'currentColor',
-  // },
 })(MaterialSlider);
